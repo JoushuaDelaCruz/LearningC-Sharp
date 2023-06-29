3563c09b4185e8ca66c27b54dec9584de6783974
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-06-29 15:01:13 -0700

    Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        /** 
+         * Drives the main program.
+         * 
+         * @param args: unused
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
